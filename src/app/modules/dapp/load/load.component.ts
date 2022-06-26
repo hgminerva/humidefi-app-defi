40653e43389e8e9191e2053f1bcc6834f8fef1cc
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2022-06-27 00:56:52 +0800

    gcash updates

--- src/app/modules/dapp/load/load.component.ts
+++ src/app/modules/dapp/load/load.component.ts
@@ -1,4 +1,6 @@
+import { DecimalPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AppSettings } from 'src/app/app-settings';
 import { PolkadotService } from 'src/app/services/polkadot/polkadot.service';
 
 @Component({
@@ -9,10 +11,17 @@ import { PolkadotService } from 'src/app/services/polkadot/polkadot.service';
 export class LoadComponent implements OnInit {
 
   constructor(
-    private polkadotService: PolkadotService
+    private polkadotService: PolkadotService,
+    private appSettings: AppSettings,
+    public decimalPipe: DecimalPipe
   ) { }
 
   walletKeyPair: string = "";
+
+  gcashSourceTokens: string[] = [];
+  gcashSelectedSourceToken: string = "";
+  gcashTokenQuantity: number = 0;
+  totalAmount: string = "0.00";
 
   networkSource: string[] = ['Acala', 'Humidefi'];
   selectedNetworkSource: string = "Acala";
@@ -26,6 +35,9 @@ export class LoadComponent implements OnInit {
   async getChainTokens(): Promise<void> {
     let tokens: Promise<string[]> = this.polkadotService.getChainTokens(this.walletKeyPair);
 
+    this.gcashSourceTokens = (await tokens);
+    this.gcashSelectedSourceToken = this.sourceTokens[0];
+
     this.sourceTokens = (await tokens);
     this.selectedSourceToken = this.sourceTokens[0];
 
@@ -33,9 +45,27 @@ export class LoadComponent implements OnInit {
     this.selectedDestinationToken = this.destinationTokens[0];
   }
 
+  gcashSourceTickerOnChange(event: any): void {
+    this.getPhpValue();
+  }
+
+  gcashSourceQuantityOnKeyup(event: any): void {
+    this.getPhpValue();
+  }
+
+  getPhpValue(): void {
+    let phpCurrency = this.appSettings.currencies.filter(d => d.currency == "PHP")[0];
+    if (phpCurrency != null) {
+      let selectedToken = phpCurrency.tokensPrices.filter(d => d.token == this.gcashSelectedSourceToken)[0];
+      if (selectedToken != null) {
+        let phpValue = this.gcashTokenQuantity * selectedToken.price;
+        this.totalAmount = this.decimalPipe.transform(phpValue, "1.2-2") || "0";
+      }
+    }
+  }
+
   ngOnInit(): void {
     this.walletKeyPair = localStorage.getItem("wallet-keypair") || "";
     this.getChainTokens();
   }
-
 }
